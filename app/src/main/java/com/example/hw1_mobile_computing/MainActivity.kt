ee7228de7736e2e205d148b23419cd8f7a9ecc19
Kiisshen <karnajuuso@gmail.com>
2026-02-15 16:06:33 +0200

    HW4 done.

--- app/src/main/java/com/example/hw1_mobile_computing/MainActivity.kt
+++ app/src/main/java/com/example/hw1_mobile_computing/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.hw1_mobile_computing
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -41,12 +44,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import android.net.Uri
-import androidx.compose.runtime.DisposableEffect
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import coil3.compose.rememberAsyncImagePainter
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 
 @Serializable
 object Main
@@ -57,9 +67,17 @@ object More
 @Serializable
 object Change
 
+const val CHANNEL_ID = "shake_notification_channel"
+const val NOTIFICATION_ID = 1
 const val PREFS_NAME = "com.example.hw1_mobile_computing.PREFERENCES"
 const val KEY_MAIN_TITLE = "main_title"
 const val KEY_MAIN_IMAGE_URI  = "main_image_uri"
+
+private var lastX = 0f
+private var lastY = 0f
+private var lastZ = 0f
+private var isFirstReading = true
+private var lastShakeTime: Long = 0
 
 fun saveMainTitle(context: Context, title: String) {
     val sharedPref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -88,14 +106,49 @@ fun loadMainImageUri(context: Context): String? {
 }
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNotificationChannel(this)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
         setContent {
             HW1_Mobile_computingTheme {
-                AppWithNavigator()
+                AppWithNavigator(
+                    onEnableSensor = {
+                        sendShakeNotification(this)
+                        startListening()
+                    }
+                )
             }
         }
+    }
+
+    private val sensorListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            if (isShake(event)) {
+                sendShakeNotification(this@MainActivity)
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    }
+
+    private fun startListening() {
+        accelerometer?.let {
+            sensorManager.registerListener(sensorListener, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(sensorListener)
     }
 }
 
@@ -173,10 +226,94 @@ fun SettingsButton(
     }
 }
 
+fun isShake(event: SensorEvent): Boolean {
+    val x = event.values[0]
+    val y = event.values[1]
+    val z = event.values[2]
+
+    if (isFirstReading) {
+        lastX = x
+        lastY = y
+        lastZ = z
+        isFirstReading = false
+        return false
+    }
+
+    val deltaX = Math.abs(lastX - x)
+    val deltaY = Math.abs(lastY - y)
+    val deltaZ = Math.abs(lastZ - z)
+
+    lastX = x
+    lastY = y
+    lastZ = z
+
+    val currentTime = System.currentTimeMillis()
+
+    if ((deltaX > 0.5f || deltaY > 0.5f || deltaZ > 0.5f) &&
+        currentTime - lastShakeTime > 1000) {
+
+        lastShakeTime = currentTime
+        return true
+    }
+
+    return false
+}
+
+private fun sendShakeNotification(context: Context) {
+    val resultIntent = Intent(context, MainActivity::class.java)
+
+    val resultPendingIntent: PendingIntent? =
+        TaskStackBuilder.create(context).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.android_logo)
+        .setContentTitle("Shake Detected!")
+        .setContentText("You moved your device quickly.")
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setContentIntent(resultPendingIntent)
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                context as ComponentActivity,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                101
+            )
+            return@with
+        }
+        notify(NOTIFICATION_ID, builder.build())
+    }
+}
+
+private fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = context.getString(R.string.channel_name)
+        val descriptionText = context.getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
 @Composable
 fun MainScreen(
     onNavigateToMore: () -> Unit,
-    onNavigateToChangeInfo: () -> Unit
+    onNavigateToChangeInfo: () -> Unit,
+    onEnableSensor: () -> Unit
 ) {
     HW1_Mobile_computingTheme {
 
@@ -246,6 +383,10 @@ fun MainScreen(
                 Spacer(modifier = Modifier.padding(top = 10.dp))
                 Button("See more lorem ipsum!") {
                     onNavigateToMore()
+                }
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button("Allow use of accelerometer sensor.") {
+                    onEnableSensor()
                 }
             }
         }
@@ -340,31 +481,31 @@ fun ChangeInfoScreen(
     }
 }
 @Composable
-fun AppWithNavigator() {
+fun AppWithNavigator(
+    onEnableSensor: () -> Unit
+) {
     val navController = rememberNavController()
+
     NavHost(navController, startDestination = Main) {
         composable<Main> {
-            MainScreen (
+            MainScreen(
                 onNavigateToMore = {
                     navController.navigate(route = More)
                 },
                 onNavigateToChangeInfo = {
                     navController.navigate(route = Change)
-                }
+                },
+                onEnableSensor = onEnableSensor
             )
         }
         composable<More> {
             MoreScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                }
+                onNavigateBack = { navController.popBackStack() }
             )
         }
         composable<Change> {
             ChangeInfoScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                }
+                onNavigateBack = { navController.popBackStack() }
             )
         }
     }
